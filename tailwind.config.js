93f1a635f01c1854f24c6cca0ac26be617a8f498
umutice0 <umutefe3452ordu@gmail.com>
2024-06-16 10:11:48 +0300

    Özel fontlar ve renkler öğrenildi. Ayrıca container'ı mx-auto kullanmadan nasıl ortalamak için kullanabileceğim öğrenildi.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true
+    },
+    extend: {
+      fontFamily: {
+        ubuntu: ['Ubuntu']
+      },
+
+      colors: {
+        ab: "#19aae3" 
+      }
+    },
   },
   plugins: [],
 }
 
+// eğer ab-cd şeklinde tanımlanacaksa tırnak içine alınmalı
